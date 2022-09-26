f9921f55cf9ab8dfc0134414d57a1511cf409aab
dzenankosuta <dzenan_kosuta@hotmail.com>
2022-09-26 21:01:45 +0200

    domaci + pocetak number tip podatka

---
+++ JavaScript/20. Cas/app.js
@@ -0,0 +1,10 @@
+// Number tip podatka
+
+// JavaScript poznaje samo number tip podatka.
+// I brojevi mogu biti zapisani sa ili bez decimalnog zapisa.
+
+// U JavaScriptu aritmeticke operaciju nisu uvek 100% tacne. IMATI NA UMU!!!
+let a = 0.2;
+let b = 0.1;
+let c = a + b;
+console.log(c);
