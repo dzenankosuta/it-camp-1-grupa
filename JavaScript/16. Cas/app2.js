4aede3318d8012a957ad0ce37f59289c2bee456b
dzenankosuta <dzenan_kosuta@hotmail.com>
2022-11-24 17:27:42 +0100

    after removing cache

--- JavaScript/16. Cas/app2.js
+++
@@ -1,7 +0,0 @@
-const brojFlasa = function () {
-  const zapreminaJedne = +prompt("Unesite zapreminu flase: ");
-  const ukupnaKolicina = +prompt("Unesite kolicinu limunade: ");
-  return Math.ceil(ukupnaKolicina / zapreminaJedne);
-};
-
-console.log(brojFlasa());
