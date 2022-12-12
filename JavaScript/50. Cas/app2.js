afc4fecd6d841d05d3d0cb83241594705aac373c
dzenankosuta <dzenan_kosuta@hotmail.com>
2022-12-12 20:42:11 +0100

    modules

---
+++ JavaScript/50. Cas/app2.js
@@ -0,0 +1,13 @@
+// JavaScript moduli (modules) //
+
+// Moduli nam dozvoljavaju da podelimo nas kod u vise fajlova.
+// Za pravljenje modula se koriste
+// import i
+// export
+// statements (naredbe).
+
+import zbir from "./functions.js";
+import { br } from "./functions.js";
+
+console.log(zbir(4, 6));
+console.log(br);
---
+++ JavaScript/50. Cas/functions.js
@@ -0,0 +1,14 @@
+const zbir = function (arg1, arg2) {
+  return arg1 + arg2;
+};
+
+const br = 5;
+
+export default zbir;
+export { br };
+
+// Postoje 2 nacina za eksportovanje modula:
+// export zbir
+// export default zbir
+
+// export default mozemo iskoristiti samo jednom u fajlu.
